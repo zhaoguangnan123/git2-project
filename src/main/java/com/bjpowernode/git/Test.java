b4bd85482fe14b3f18d5a00cdb8fdbbd503a6a55
zhaoguangnan123 <z505047496@163.com>
2021-01-23 17:18:34 +0800

    修改helloworld 22222

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -7,5 +7,6 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("2222");
     }
 }
